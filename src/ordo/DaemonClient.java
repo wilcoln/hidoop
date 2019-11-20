6793bb4c84013dfb3b8567a0aadd689975846ceb
Wilfried Bounsi <wilcoln99@gmail.com>
2019-11-20 01:04:44 +0100

    update DaemonClient

--- src/ordo/DaemonClient.java
+++ src/ordo/DaemonClient.java
@@ -8,6 +8,7 @@ public class DaemonClient {
     public static void main(String args[]){
         try {
             Daemon obj = (Daemon) Naming.lookup("//192.168.122.1:" + Project.RMIREGISTRY_PORT + "/mapred");
+            System.out.println(obj);
             System.out.println(obj.test());
         }catch (Exception e){
             e.printStackTrace();
