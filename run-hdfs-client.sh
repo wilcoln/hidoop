8b12b09f59137f32eecc1b22f5a6732dbdc5bcb6
Wilfried Bounsi <wilcoln99@gmail.com>
2019-12-03 20:50:07 +0100

    update scripts

--- run-hdfs-client.sh
+++ run-hdfs-client.sh
@@ -1,4 +1,4 @@
 cd src
 javac */*.java
-java -Djava.rmi.server.hostname=$1 hdfs.HdfsClient
+java hdfs.HdfsClient
 rm -rf */*.class
--- run-worker.sh
+++ run-worker.sh
@@ -1,3 +1,3 @@
 cd src
 javac */*.java
-java -Djava.rmi.server.hostname=$1 ordo.MapWorker
+java ordo.MapWorker
