37f3498f12151cf607db8c31333b2cf1674d5380
Wilfried Bounsi <wilcoln99@gmail.com>
2019-12-03 14:08:35 +0100

    change config file for distribution test

---
+++ src/config/Config.java
@@ -0,0 +1,21 @@
+package config;
+
+import utils.Node;
+
+import java.util.ArrayList;
+
+public class Config {
+    public static String PATH="/home/wilcoln/Workspace/School/Semestre 1/Systèmes Concurrents/hidoop";
+    public static String OUTPUT_PATH="/home/wilcoln/Workspace/School/Semestre 1/Systèmes Concurrents/hidoop/output";
+    public static String FRAGMENTS_PATH = "./fragments";
+    public static int HDFS_SERVER_PORT = 3333;
+    public static int RMIREGISTRY_PORT = 5021;
+    public static Node master;
+    public static ArrayList<Node> workers;
+    static {
+        master = new Node("interface", "192.168.122.1");
+        workers = new ArrayList<>();
+        workers.add(master);
+        workers.add(new Node("master", "192.168.122.216"));
+    }
+}
