8afe708bb009460071d8b1ea1de2af88c2d1d03f
Wilfried Bounsi <wilcoln99@gmail.com>
2020-01-11 23:28:24 +0100

    remove java doc from git tracking

--- bin/start.sh
+++
@@ -1,7 +0,0 @@
-javac -d . ../src/*/*.java
-
-gnome-terminal -x sh -c "java hdfs.NameNode"
-sleep 0.25
-gnome-terminal -x sh -c "java hdfs.DataNode"
-sleep 0.25
-gnome-terminal -x sh -c "java ordo.MapWorker"
