6c4560b419c434dc39442252a05f7d4f00879864
Wilfried Bounsi <wilcoln99@gmail.com>
2020-01-12 17:54:19 +0100

    add bin to git tracking

---
+++ bin/stop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Colors
+orange='\033[0;33m'
+NC='\033[0m' # No Color
+
+if jps | grep NameNode >/dev/null; then
+  printf "Stopping NameNode...\n"
+  pkill -f hidoop-namenode-daemon
+  printf "${orange}NameNode Stopped${NC}\n"
+else
+  printf "No Master running on host.\n"
+fi
+
+if jps | grep DataNode >/dev/null; then
+
+  printf "Stopping DataNode...\n"
+  pkill -f datanode-daemon
+  printf "${orange}DataNode Stopped${NC}\n"
+
+  printf "Stopping MapWorker...\n"
+  pkill -f mapworker-daemon
+  printf "${orange}MapWorker Stopped${NC}\n"
+else
+  printf "No Worker running on host.\n"
+fi
