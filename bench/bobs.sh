ffcc08e361d373ad4ddc81fef30264c07cbfbb71
Bounsi djatcheu Wilfried <wbounsid@ackbar.enseeiht.fr>
2020-02-15 07:24:21 +0100

    restructure folder

--- bench/bobs.sh
+++ bench/bobs.sh
@@ -11,23 +11,23 @@ function bench_on_bs {
     do
         factor=$(echo "2^"$power | bc)
         bs=$(expr $factor \* 1000000)
-        $HIDOOP_HOME/bench/change-bs.sh $bs
-        echo "### WITH BLOC SIZE = "$bs &>> $bobs_results
-        hidoop bench mb $input_size &>> $bobs_results
+        $HIDOOP_HOME/bench/utils/set-bs.sh $bs
+        echo "### WITH BLOC SIZE = "$bs &>> $results
+        hidoop bench mb $input_size &>> $results
     done
 }
 
 # On crée le fichier résultat
-bobs_results=$HIDOOP_HOME/bench/bobs_results
-rm $bobs_results
-touch $bobs_results
+results=$HIDOOP_HOME/bench/results/bobs
+rm $results
+touch $results
 
 # On fixe la taille du cluster
 cp $HIDOOP_HOME/bench/5w.xml $HIDOOP_HOME/config/core-site.xml
-echo "# FIXED ClUSTER SIZE : 5 workers" &>> $bobs_results
+echo "# FIXED ClUSTER SIZE : 5 workers" &>> $results
 
 # On fixe la taille du fichier d'entrée
-echo "# FIXED INPUT SIZE : 512MB" &>> $bobs_results
+echo "# FIXED INPUT SIZE : 512MB" &>> $results
 
 # On fait varier la taille des blocs
 start=0;end=9;input_size=512
@@ -35,7 +35,7 @@ bench_on_bs
 
 
 # On fixe la taille du fichier d'entrée à 1GB
-echo "# FIXED INPUT SIZE : 1GB" &>> $bobs_results
+echo "# FIXED INPUT SIZE : 1GB" &>> $results
 
 # On fait varier la taille des blocs
 start=1;end=10;input_size=1024
--- bench/bocs.sh
+++ bench/bocs.sh
@@ -3,16 +3,16 @@
 # bocs : Bench On Cluster Size
 
 # On crée le fichier résultat
-bocs_results=$HIDOOP_HOME/bench/bocs_results
-rm $bocs_results
-touch $bocs_results
+results=$HIDOOP_HOME/bench/results/bocs
+rm $results
+touch $results
 
 # On fixe la taille de blocs
-$HIDOOP_HOME/bench/change-bs.sh 128000000
-echo "# FIXED BLOC SIZE : 128MB" &>> $bocs_results
+$HIDOOP_HOME/bench/utils/set-bs.sh 128000000
+echo "# FIXED BLOC SIZE : 128MB" &>> $results
 
 # On fixe la taille du fichier d'entrée
-echo "# FIXED INPUT SIZE : 2GB" &>> $bocs_results
+echo "# FIXED INPUT SIZE : 2GB" &>> $results
 
 # On fait varier la taille du cluster
 for i in {1..5}
@@ -20,7 +20,7 @@ do
     hidoop clean
     hidoop stop
     cp $HIDOOP_HOME/bench/${i}w.xml $HIDOOP_HOME/config/core-site.xml
-    echo "## WITH $i WORKER(S)" &>> $bocs_results
+    echo "## WITH $i WORKER(S)" &>> $results
     hidoop start
-    hidoop bench gb 2 &>> $bocs_results
+    hidoop bench gb 2 &>> $results
 done
