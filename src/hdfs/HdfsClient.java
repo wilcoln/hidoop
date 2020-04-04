9bd8d355cc316ec25b5618dec25e18e762968bea
Elmontassir Rachid <relmonta@sar.enseeiht.fr>
2020-04-04 23:16:27 +0200

    f

--- src/hdfs/HdfsClient.java
+++ src/hdfs/HdfsClient.java
@@ -219,7 +219,7 @@ public class HdfsClient implements HdfsClientIt {
 				usage();
 				return;
 			}
-			hc.HdfsWrite(fmt, args[2], 1);
+			hc.HdfsWrite(fmt, args[2], 2);
 			break;
 		case "read":
 			hc.HdfsRead(args[1], args[2]);
