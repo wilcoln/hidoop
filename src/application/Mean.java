d4c1e55a045571c0baf4b403552a748128513e53
relmo <elmontassir.rachid1937@gmail.com>
2020-04-06 12:48:55 +0200

    OK

--- src/application/Mean.java
+++ src/application/Mean.java
@@ -1,60 +1,38 @@
 package application;
+ 
+import java.io.*;
+public class Mean {
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+	public static void main(String[] args) {
 
-import map.MapReduce;
-import ordo.Job;
-import formats.Format;
-import formats.FormatReader;
-import formats.FormatWriter;
-import formats.KV;
+		try {
+            long t1 = System.currentTimeMillis();
 
-public class Mean implements MapReduce {
-	private static final long serialVersionUID = 1L;
-
-	// MapReduce program that computes word counts
-	public void map(FormatReader reader, FormatWriter writer) {
-		long sum = 0L;
-		int nbElem = 0;
-		KV kv;
-		while ((kv = reader.read()) != null) {
-			StringTokenizer st = new StringTokenizer(kv.v);
-			while (st.hasMoreTokens()) {
-				String tok = st.nextToken();
-				if (!tok.equals("")) {
-					sum += ((long) Float.parseFloat(tok));
-					nbElem++;
-				}
+			long sum = 0L;
+			int nbElem = 0;
+			LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(args[0])));
+			while (true) {
+				String l = reader.readLine();
+                if (l == null) break;
+                String[] list = l.split(" ");
+                for (String elt : list){
+				    if (!elt.equals("")) {
+						sum += ((long) Float.parseFloat(elt));
+						nbElem++;
+                    }
+                }
 			}
+			File resultFile = new File("sum.out."+args[0]);
+			resultFile.createNewFile();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultFile, false)));
+			writer.write("mean<->"+sum/((float) nbElem));
+			writer.newLine();
+			writer.close();
+			reader.close();
+            long t2 = System.currentTimeMillis();
+            System.out.println("time in ms ="+(t2-t1));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		writer.write(new KV(""+nbElem,sum+""));
 	}
-	
-	public void reduce(FormatReader reader, FormatWriter writer) {
-		KV kv;
-		long sum = 0L;
-		int nbElem = 0;
-		while ((kv = reader.read()) != null) {
-			if (!kv.v.equals("")){
-				sum += Float.parseFloat(kv.v);
-				nbElem += Integer.parseInt(kv.k);
-			}
-		}
-		nbElem = nbElem==0? 0 : nbElem;
-		writer.write(new KV("Mean",sum/((float) nbElem)+""));
-	}
-	
-	public static void main(String[] args) throws Exception {
-		Job j = new Job();
-        j.setInputFormat(Format.Type.LINE);
-        j.setInputFname(args[0]);
-        long t1 = System.currentTimeMillis();
-		j.startJob(new Mean());
-		long t2 = System.currentTimeMillis();
-        System.out.println("time in ms ="+(t2-t1));
-        System.exit(0);
-
-		}
 }
---
+++ src/application/MeanMR.java
@@ -0,0 +1,58 @@
+package application;
+
+import java.util.StringTokenizer;
+
+import map.MapReduce;
+import ordo.Job;
+import formats.Format;
+import formats.FormatReader;
+import formats.FormatWriter;
+import formats.KV;
+
+public class MeanMR implements MapReduce {
+	private static final long serialVersionUID = 1L;
+
+	// MapReduce program that computes word counts
+	public void map(FormatReader reader, FormatWriter writer) {
+		long sum = 0L;
+		int nbElem = 0;
+		KV kv;
+		while ((kv = reader.read()) != null) {
+			StringTokenizer st = new StringTokenizer(kv.v);
+			while (st.hasMoreTokens()) {
+				String tok = st.nextToken();
+				if (!tok.equals("")) {
+					sum += ((long) Float.parseFloat(tok));
+					nbElem++;
+				}
+			}
+		}
+		writer.write(new KV(""+nbElem,sum+""));
+	}
+	
+	public void reduce(FormatReader reader, FormatWriter writer) {
+		KV kv;
+		long sum = 0L;
+		int nbElem = 0;
+		while ((kv = reader.read()) != null) {
+			if (!kv.v.equals("")){
+				sum += Float.parseFloat(kv.v);
+				nbElem += Integer.parseInt(kv.k);
+			}
+		}
+		nbElem = nbElem==0? 0 : nbElem;
+		writer.write(new KV("Mean",sum/((float) nbElem)+""));
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Job j = new Job();
+        j.setInputFormat(Format.Type.LINE);
+        j.setInputFname(args[0]);
+        long t1 = System.currentTimeMillis();
+		j.startJob(new MeanMR());
+		long t2 = System.currentTimeMillis();
+        System.out.println("time in ms ="+(t2-t1));
+        System.exit(0);
+
+		}
+}
